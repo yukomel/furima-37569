3efd84d7edbc4a72fcfda96e38b60d84d2f878d7
yukomel <ym02101213@gmail.com>
2022-07-03 22:59:16 +0900

    ordersコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:new, :create, :update, :index, :show, :edit, :destroy] do
+  resources :products do
+    resources :orders, only:[:index, :create]
   end
 end
