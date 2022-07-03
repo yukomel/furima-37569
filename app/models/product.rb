6b024f78349012a3315ffbd21c62807606ca609e
yukomel <ym02101213@gmail.com>
2022-07-03 11:28:30 +0900

    修正&menuモデルの削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -22,6 +22,6 @@ class Product < ApplicationRecord
 
   validates :category_id, :status_id, :shopping_cost_id, :prefecture_id, :shopping_day_id,
             numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  
 end
