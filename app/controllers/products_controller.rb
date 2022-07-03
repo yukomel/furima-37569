d229277fc229c7069ae119dbdf34edd6cd0153c4
yukomel <ym02101213@gmail.com>
2022-07-03 21:46:04 +0900

    修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,9 +41,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    product = Product.find(params[:id])
-    if product.user_id == current_user.id
-      product.destroy
+      @product.destroy
       redirect_to root_path
     end
   end
