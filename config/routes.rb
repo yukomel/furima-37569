8a6988ed7195755cd631d3ab0528e2314b263627
yukomel <ym02101213@gmail.com>
2022-07-03 17:15:01 +0900

    条件分岐

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:new, :create, :update, :index ] do
+  resources :products, only: [:new, :create, :update, :index, :show] do
   end
 end
