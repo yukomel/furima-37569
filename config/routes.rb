30107571a808985bc3975cf3dc881c705c546b5c
yukomel <ym02101213@gmail.com>
2022-07-03 21:13:12 +0900

    Revert "Revert "アクションの定義""
    This reverts commit f82b5cfe02d0e2a42b92e81bf551189b59dfe03c.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:new, :create, :update, :index, :show, :edit] do
+  resources :products, only: [:new, :create, :update, :index, :show, :edit, :destroy] do
   end
 end
